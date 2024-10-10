21594dd01fc31c4e730b9e84596a51ec7679133e
WilliamHangXu <willxh.68209@gmail.com>
2024-10-10 15:26:04 -0500

    comparison

--- examples/multitag.jl
+++ examples/multitag.jl
@@ -219,221 +219,264 @@ function make_multitag_prior(zero_state, num_p, num_e; n=16)
 end
 
 function test_multitag(num_p=2, num_e=2)
-    T = 10
-    n_particles=1000
+    for solvers in [
+        (:active, :inactive, :inactive, :inactive),
+        (:active, :inactive, :active, :active)]
 
-    fov = NamedTuple()
-    for i = 1:num_p
-        p = Symbol("p$(i)")
-        fov = Base.merge(fov, (p => 1.0,))
-    end
-    for j = 1:num_e
-        e = Symbol("e$(j)")
-        fov = Base.merge(fov, (e => 1.0,))
-    end
+        println("=======\nP1 $(solvers[1]), P2 $(solvers[2]), E1 $(solvers[3]), E2 $(solvers[4])")
 
+        for s = 1:50
+            print(s, " ")
+            Random.seed!(42+s)
 
-    optimization_options = (;
-        n_lookahead = T,
-        n_iters = 5,
-        batch_size = 20,
-        max_wall_time = 120000, # Not respected right now
-        steps_per_seed = 1,
-        steps_per_render = 10
-    )
+            T = 7
+            n_particles=1000
 
-    clock_step = make_clock_step(1.0)
-    pos_hist_step = NamedTuple()
-    obs_hist_step = NamedTuple()
-    ang_hist_step = NamedTuple()
-    dyn_step = NamedTuple()
-    bound_step = NamedTuple()
-    obs_step = NamedTuple()
-    control_step = NamedTuple()
-
-    for i = 1:num_p
-        p = Symbol("p$(i)")
-
-        # pos_hist_step
-        p_pos_h = Symbol("p$(i)_pos_h")
-        p_pos = Symbol("p$(i)_pos")
-        pos_hist_step = Base.merge(pos_hist_step, (p => make_hist_step(p_pos_h, p_pos, T),))
-
-        # obs_hist_step
-        p_obs_h = Symbol("p$(i)_obs_h")
-        p_obs = Symbol("p$(i)_obs")
-        obs_hist_step = Base.merge(obs_hist_step, (p => make_hist_step(p_obs_h, p_obs, T),))
-
-        # ang_hist_step
-        p_θ_h = Symbol("p$(i)_θ_h")
-        p_θ = Symbol("p$(i)_θ")
-        ang_hist_step = Base.merge(ang_hist_step, (p => make_hist_step(p_θ_h, p_θ, T),))
-
-        # dyn_step
-        dyn_step = Base.merge(dyn_step, (p => make_vel_dynamics_step(p; control_scale=1.4),))
-
-        # bound_step
-        bound_step = Base.merge(bound_step, (p => make_bound_step(p_pos, -42, 42),))
-
-        # obs_step
-        obs_step = Base.merge(obs_step, (p => make_multitag_sensing_step(p, num_p, num_e; fov=fov[p]),))
-
-        # control_step
-        p_vel = Symbol("p$(i)_vel")
-        control_step = Base.merge(control_step, (p => make_nn_control(p, [p_pos_h, p_obs_h], p_vel, t_max=T),))
-
-    end
-
-    for j = 1:num_e
-        e = Symbol("e$(j)")
-
-        # pos_hist_step
-        e_pos_h = Symbol("e$(j)_pos_h")
-        e_pos = Symbol("e$(j)_pos")
-        pos_hist_step = Base.merge(pos_hist_step, (e => make_hist_step(e_pos_h, e_pos, T),))
-
-        # obs_hist_step
-        e_obs_h = Symbol("e$(j)_obs_h")
-        e_obs = Symbol("e$(j)_obs")
-        obs_hist_step = Base.merge(obs_hist_step, (e => make_hist_step(e_obs_h, e_obs, T),))
-
-        # ang_hist_step
-        e_θ_h = Symbol("e$(j)_θ_h")
-        e_θ = Symbol("e$(j)_θ")
-        ang_hist_step = Base.merge(ang_hist_step, (e => make_hist_step(e_θ_h, e_θ, T),))
-
-        # dyn_step
-        dyn_step = Base.merge(dyn_step, (e => make_vel_dynamics_step(e; control_scale=2),))
-
-        # bound_step
-        bound_step = Base.merge(bound_step, (e => make_bound_step(e_pos, -42, 42),))
-
-        # obs_step
-        obs_step = Base.merge(obs_step, (e => make_multitag_sensing_step(e, num_p, num_e; fov=fov[e]),))
-
-        # control_step
-        e_vel = Symbol("e$(j)_vel")
-        control_step = Base.merge(control_step, (e => make_nn_control(e, [e_pos_h, e_obs_h], e_vel, t_max=T),))
-
-    end
-
-    attr_ids = [:pos, :vel, :acc, :θ, :vω, :obs,                 :pos_h, :obs_h,                 :θ_h]
-    attr_w   = [ 2;    2;    2;    1;  2;   2*(num_p+num_e-1);    2*T;    2*(num_p+num_e-1)*T;    T]
-    zero_state_arg = [:t => 1]
-    for i = 1:num_p
-        p = Symbol("p$(i)")
-        append!(zero_state_arg, Symbol.(p, :_, attr_ids) .=> attr_w)
-    end
-    for j = 1:num_e
-        e = Symbol("e$(j)")
-        append!(zero_state_arg, Symbol.(e, :_, attr_ids) .=> attr_w)
-    end
-
-    zero_state = State(zero_state_arg...)
+            fov = NamedTuple()
+            for i = 1:num_p
+                p = Symbol("p$(i)")
+                fov = Base.merge(fov, (p => 1.0,))
+            end
+            for j = 1:num_e
+                e = Symbol("e$(j)")
+                fov = Base.merge(fov, (e => 1.0,))
+            end
 
 
-    multitag_game = ContinuousGame([
-        clock_step,
-        # These are all named tuples (player symbol => func)
-        values(obs_step)...,
-        values(pos_hist_step)...,
-        # values(obs_hist_step)...,
-        values(ang_hist_step)...,
-        values(control_step)...,
-        values(dyn_step)...,
-    ])
+            optimization_options = (;
+                n_lookahead = T,
+                n_iters = 20,
+                batch_size = 5,
+                max_wall_time = 120000, # Not respected right now
+                steps_per_seed = 1,
+                steps_per_render = 10
+            )
 
-    prior_fn = make_multitag_prior(zero_state, num_p, num_e, n=n_particles)
+            clock_step = make_clock_step(1.0)
+            pos_hist_step = NamedTuple()
+            obs_hist_step = NamedTuple()
+            ang_hist_step = NamedTuple()
+            dyn_step = NamedTuple()
+            bound_step = NamedTuple()
+            obs_step = NamedTuple()
+            control_step = NamedTuple()
+            obs_hist_step_planning = NamedTuple()
 
-    # Shared brain: Everyone agrees on the belief
-    joint_belief = JointParticleBelief(multitag_game, prior_fn())
+            for i = 1:num_p
+                p = Symbol("p$(i)")
 
-    # <Mel> Aside: I added some explanation of what these <X>ParticleBeliefs mean
-    #   over in `beliefs.jl`.
+                # pos_hist_step
+                p_pos_h = Symbol("p$(i)_pos_h")
+                p_pos = Symbol("p$(i)_pos")
+                pos_hist_step = Base.merge(pos_hist_step, (p => make_hist_step(p_pos_h, p_pos, T),))
 
-    # Separate brains: Everyone has their own belief
-    # beliefs = NamedTuple()
-    # for i = 1:(num_p)
-    #     p = Symbol("p$(i)")
-    #     beliefs = Base.merge(beliefs, (p => HybridParticleBelief(fovtag_game, prior_fn(), 0.01),))
-    # end
-    # for j = 1:(num_e)
-    #     e = Symbol("e$(j)")
-    #     beliefs = Base.merge(beliefs, (e => HybridParticleBelief(fovtag_game, prior_fn(), 0.01),))
-    # end
+                # obs_hist_step
+                p_obs_h = Symbol("p$(i)_obs_h")
+                p_obs = Symbol("p$(i)_obs")
+                p_obs_hist_step = make_hist_step(p_obs_h, p_obs, T)
+                obs_hist_step = Base.merge(obs_hist_step, (p => p_obs_hist_step,))
+
+                # obs_hist_step_planning
+                solver = solvers[i]
+                p_obs_hist_step_planning = (solver == :active) ? p_obs_hist_step : make_identity_step()
+                obs_hist_step_planning = Base.merge(obs_hist_step_planning, (p => p_obs_hist_step_planning,))
+
+                # ang_hist_step
+                p_θ_h = Symbol("p$(i)_θ_h")
+                p_θ = Symbol("p$(i)_θ")
+                ang_hist_step = Base.merge(ang_hist_step, (p => make_hist_step(p_θ_h, p_θ, T),))
+
+                # dyn_step
+                dyn_step = Base.merge(dyn_step, (p => make_vel_dynamics_step(p; control_scale=1.4),))
+
+                # bound_step
+                bound_step = Base.merge(bound_step, (p => make_bound_step(p_pos, -42, 42),))
+
+                # obs_step
+                obs_step = Base.merge(obs_step, (p => make_multitag_sensing_step(p, num_p, num_e; fov=fov[p]),))
+
+                # control_step
+                p_vel = Symbol("p$(i)_vel")
+                control_step = Base.merge(control_step, (p => make_nn_control(p, [p_pos_h, p_obs_h], p_vel, t_max=T),))
+
+            end
+
+            for j = 1:num_e
+                e = Symbol("e$(j)")
+
+                # pos_hist_step
+                e_pos_h = Symbol("e$(j)_pos_h")
+                e_pos = Symbol("e$(j)_pos")
+                pos_hist_step = Base.merge(pos_hist_step, (e => make_hist_step(e_pos_h, e_pos, T),))
+
+                # obs_hist_step
+                e_obs_h = Symbol("e$(j)_obs_h")
+                e_obs = Symbol("e$(j)_obs")
+                e_obs_hist_step = make_hist_step(e_obs_h, e_obs, T)
+                obs_hist_step = Base.merge(obs_hist_step, (e => e_obs_hist_step,))
+
+                # obs_hist_step_planning
+                solver = solvers[num_p + j]
+                e_obs_hist_step_planning = (solver == :active) ? e_obs_hist_step : make_identity_step()
+                obs_hist_step_planning = Base.merge(obs_hist_step_planning, (e => e_obs_hist_step_planning,))
+
+                # ang_hist_step
+                e_θ_h = Symbol("e$(j)_θ_h")
+                e_θ = Symbol("e$(j)_θ")
+                ang_hist_step = Base.merge(ang_hist_step, (e => make_hist_step(e_θ_h, e_θ, T),))
+
+                # dyn_step
+                dyn_step = Base.merge(dyn_step, (e => make_vel_dynamics_step(e; control_scale=2),))
+
+                # bound_step
+                bound_step = Base.merge(bound_step, (e => make_bound_step(e_pos, -42, 42),))
+
+                # obs_step
+                obs_step = Base.merge(obs_step, (e => make_multitag_sensing_step(e, num_p, num_e; fov=fov[e]),))
+
+                # control_step
+                e_vel = Symbol("e$(j)_vel")
+                control_step = Base.merge(control_step, (e => make_nn_control(e, [e_pos_h, e_obs_h], e_vel, t_max=T),))
+
+            end
+
+            attr_ids = [:pos, :vel, :acc, :θ, :vω, :obs,                 :pos_h, :obs_h,                 :θ_h]
+            attr_w   = [ 2;    2;    2;    1;  2;   2*(num_p+num_e-1);    2*T;    2*(num_p+num_e-1)*T;    T]
+            zero_state_arg = [:t => 1]
+            for i = 1:num_p
+                p = Symbol("p$(i)")
+                append!(zero_state_arg, Symbol.(p, :_, attr_ids) .=> attr_w)
+            end
+            for j = 1:num_e
+                e = Symbol("e$(j)")
+                append!(zero_state_arg, Symbol.(e, :_, attr_ids) .=> attr_w)
+            end
+
+            zero_state = State(zero_state_arg...)
 
 
-    true_state = make_multitag_prior(zero_state, num_p, num_e, n=1)() # Just updated; not tested
+            multitag_game = ContinuousGame([
+                clock_step,
+                # These are all named tuples (player symbol => func)
+                values(obs_step)...,
+                values(pos_hist_step)...,
+                values(obs_hist_step)...,
+                values(ang_hist_step)...,
+                values(control_step)...,
+                values(dyn_step)...,
+            ])
 
-    ids = NamedTuple()
-    for i = 1:(num_p)
-        p = Symbol("p$(i)")
-        p_pos = Symbol("p$(i)_pos")
-        p_vel = Symbol("p$(i)_vel")
-        p_θ = Symbol("p$(i)_θ")
-        p_obs = Symbol("p$(i)_obs")
-        p_pos_h = Symbol("p$(i)_pos_h")
-        p_obs_h = Symbol("p$(i)_obs_h")
-        ids = Base.merge(ids, (p => [:t; p_pos; p_vel; p_θ; p_obs; p_pos_h; p_obs_h],))
-    end
-    for j = 1:(num_e)
-        e = Symbol("e$(j)")
-        e_pos = Symbol("e$(j)_pos")
-        e_vel = Symbol("e$(j)_vel")
-        e_θ = Symbol("e$(j)_θ")
-        e_obs = Symbol("e$(j)_obs")
-        e_pos_h = Symbol("e$(j)_pos_h")
-        e_obs_h = Symbol("e$(j)_obs_h")
-        ids = Base.merge(ids, (e => [:t; e_pos; e_vel; e_θ; e_obs; e_pos_h; e_obs_h],))
-    end
+            planning_multitag_game = ContinuousGame([
+                clock_step,
+                # These are all named tuples (player symbol => func)
+                values(obs_step)...,
+                values(pos_hist_step)...,
+                values(obs_hist_step_planning)...,
+                values(ang_hist_step)...,
+                values(control_step)...,
+                values(dyn_step)...,
+            ])
 
-    cost_fns  = make_multitag_costs(num_p, num_e)
-    renderer = MakieRenderer()
+            prior_fn = make_multitag_prior(zero_state, num_p, num_e, n=n_particles)
+
+            # Shared brain: Everyone agrees on the belief
+            joint_belief = JointParticleBelief(multitag_game, prior_fn())
+
+            # <Mel> Aside: I added some explanation of what these <X>ParticleBeliefs mean
+            #   over in `beliefs.jl`.
+
+            # Separate brains: Everyone has their own belief
+            # beliefs = NamedTuple()
+            # for i = 1:(num_p)
+            #     p = Symbol("p$(i)")
+            #     beliefs = Base.merge(beliefs, (p => HybridParticleBelief(fovtag_game, prior_fn(), 0.01),))
+            # end
+            # for j = 1:(num_e)
+            #     e = Symbol("e$(j)")
+            #     beliefs = Base.merge(beliefs, (e => HybridParticleBelief(fovtag_game, prior_fn(), 0.01),))
+            # end
 
 
-    # In split brain we would need one of these `params` tuples for EVERY player
-    #   (each of which would contain policy parameters for every player)
-    params = NamedTuple()
-    for i = 1:(num_p)
-        p = Symbol("p$(i)")
-        params = Base.merge(params, (p => make_policy(2*(num_p+num_e)*T, 3; t_max=T),))
-    end
-    for j = 1:(num_e)
-        e = Symbol("e$(j)")
-        params = Base.merge(params, (e => make_policy(2*(num_p+num_e)*T, 3; t_max=T),))
-    end
+            true_state = make_multitag_prior(zero_state, num_p, num_e, n=1)() # Just updated; not tested
 
-    # params = (;
-    #     p1 = make_policy(4*T, 3; t_max=T),
-    #     e1 = make_policy(4*T, 3; t_max=T)
-    # )
+            ids = NamedTuple()
+            for i = 1:(num_p)
+                p = Symbol("p$(i)")
+                p_pos = Symbol("p$(i)_pos")
+                p_vel = Symbol("p$(i)_vel")
+                p_θ = Symbol("p$(i)_θ")
+                p_obs = Symbol("p$(i)_obs")
+                p_pos_h = Symbol("p$(i)_pos_h")
+                p_obs_h = Symbol("p$(i)_obs_h")
+                ids = Base.merge(ids, (p => [:t; p_pos; p_vel; p_θ; p_obs; p_pos_h; p_obs_h],))
+            end
+            for j = 1:(num_e)
+                e = Symbol("e$(j)")
+                e_pos = Symbol("e$(j)_pos")
+                e_vel = Symbol("e$(j)_vel")
+                e_θ = Symbol("e$(j)_θ")
+                e_obs = Symbol("e$(j)_obs")
+                e_pos_h = Symbol("e$(j)_pos_h")
+                e_obs_h = Symbol("e$(j)_obs_h")
+                ids = Base.merge(ids, (e => [:t; e_pos; e_vel; e_θ; e_obs; e_pos_h; e_obs_h],))
+            end
 
-    for t in 1:1000
-        print(".")
+            cost_fns  = make_multitag_costs(num_p, num_e)
+            renderer = MakieRenderer()
 
-        # <Mel> The do block on `solve` is optional and what had been here was cruft (my bad)
-        params = solve(multitag_game, joint_belief, params, cost_fns, optimization_options)
 
-        true_state = step(multitag_game, true_state, params)
+            # In split brain we would need one of these `params` tuples for EVERY player
+            #   (each of which would contain policy parameters for every player)
+            params = NamedTuple()
+            for i = 1:(num_p)
+                p = Symbol("p$(i)")
+                params = Base.merge(params, (p => make_policy(2*(num_p+num_e)*T, 3; t_max=T),))
+            end
+            for j = 1:(num_e)
+                e = Symbol("e$(j)")
+                params = Base.merge(params, (e => make_policy(2*(num_p+num_e)*T, 3; t_max=T),))
+            end
+            scores_active = []
+            scores_inactive = []
 
-        # Shared brain
-        update(joint_belief, params)
+            # params = (;
+            #     p1 = make_policy(4*T, 3; t_max=T),
+            #     e1 = make_policy(4*T, 3; t_max=T)
+            # )
 
-        # Separate brains
-        # for i = 1:(num_p)
-        #     p = Symbol("p$(i)")
-        #     update(beliefs[p], select(true_state, ids[p]...)[1], params[p])
-        # end
-        # for j = 1:(num_e)
-        #     e = Symbol("e$(j)")
-        #     update(beliefs[e], select(true_state, ids[e]...)[1], params[e])
-        # end
-        
-        render_multitag(renderer, [
-            (draw(joint_belief; n=20), params),
-            (true_state, params)
-            ], multitag_game, keys(fov), fov; T)
-        
+            n_steps = 20
+            for t in 1:n_steps
+                
+
+                # <Mel> The do block on `solve` is optional and what had been here was cruft (my bad)
+                params = solve(planning_multitag_game, joint_belief, params, cost_fns, optimization_options)
+
+                true_state = step(multitag_game, true_state, params)
+
+                # Shared brain
+                update(joint_belief, params)
+
+                scores_active = [scores_active; min_dist([true_state[1][:p1_pos], (true_state[1][Symbol("e$(m)_pos")] for m in 1:num_e)...])]
+                scores_inactive = [scores_inactive; min_dist([true_state[1][:p2_pos], (true_state[1][Symbol("e$(n)_pos")] for n in 1:num_e)...])]
+
+                # Separate brains
+                # for i = 1:(num_p)
+                #     p = Symbol("p$(i)")
+                #     update(beliefs[p], select(true_state, ids[p]...)[1], params[p])
+                # end
+                # for j = 1:(num_e)
+                #     e = Symbol("e$(j)")
+                #     update(beliefs[e], select(true_state, ids[e]...)[1], params[e])
+                # end
+                
+                render_multitag(renderer, [
+                    (draw(joint_belief; n=20), params),
+                    (true_state, params)
+                    ], multitag_game, keys(fov), fov; T)
+                
+            end
+            println("Active: $(sum(scores_active)/n_steps), Inactive: $(sum(scores_inactive)/n_steps)")
+        end
     end
 end
 
