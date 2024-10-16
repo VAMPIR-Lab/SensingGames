ada3f2fb85a338db088b958b8d7f7aec506b9736
WilliamHangXu <willxh.68209@gmail.com>
2024-10-16 13:22:18 -0500

    clean up

--- examples/hastag.jl
+++ examples/hastag.jl
@@ -1,28 +1,13 @@
-#   `hastag` is an imperfect information pursuit-evasion game where
-#   each player has a field of view in the direction of their velocity.
-#   When the opponent is within the field of view observations are perfect;
-#   otherwise observations are very noisy (with a steep but smooth transition
-#   in between). There are solid obstacles within the space that block both 
-#   players' line of sight beyond it.
 using Dates
 
-# `fovtag` is an imperfect information pursuit-evasion game where
-#   each player has a field of view in the direction of their velocity.
-#   When the opponent is within the field of view observations are perfect;
-#   otherwise observations are very noisy (with a steep but smooth transition
-#   in between).
+# `hastag` (Hide-and-seek Tag) is a variant of field of view tag. The play area includes fixed circular obstacles 
+# that block a player's field of view. Agents receive high-variance observations for obstructed opponents in the
+# same way as those outside the field of view. Players are permitted to pass through obstacles, but receive a penalty
+# for doing so.
 
-
-# Observtions for FOVtag are as follows:
-# * If a player's field-of-view is over the opponent, they get 
-#   a near-perfect observation of the opponent's location
-#   (Gaussian with sigma=`offset`)
-# * If not, they get a random observation in bounds ([-40, 40] for both axes).
-#   The observation is trimmed Gaussian with sigma=scale*d where d is the angle
-#   in radians outside the field of view
-#   With a large scale this looks like a uniform distribution on [-40, 40]
-
+# default blocks (obstacles)
 de_blocks = [[0 10], [0 -10], [10 0], [-10 0], [20 20], [-20 20], [20 -20], [-20 -20], [0 -37], [0 37], [-37 0], [37 0]]
+# default block radii that correspond to the above list
 de_brads = [3, 3, 3, 3, 5, 5, 5, 5, 7, 7, 7, 7]
 
 function make_hastag_sensing_step(agent, other; fov, scale=100.0, offset=0.5, blocks=de_blocks, brads=de_brads)
@@ -39,41 +24,30 @@ function make_hastag_sensing_step(agent, other; fov, scale=100.0, offset=0.5, bl
     function get_gaussian_params(state_dist)
         our_pos = state_dist[id_own_pos]
         their_pos = state_dist[id_other_pos]
-        # fov and x axis
+        # The angle between heading and the x axis
         θ1 = state_dist[id_own_θ] .+ π
-        # two players, and x axis
+        # The angle between 1. the line connecting two players, and 2. the x axis
         θ2 = atan.(our_pos[:, 2] .- their_pos[:, 2], our_pos[:, 1] .- their_pos[:, 1])
-        # us to center of obs, and x axis
-        # θ3 = atan.(our_pos[:, 2] .- block[2], our_pos[:, 1] .- block[1])
+        # The angle between 1. the line connecting us to center of block, and 2. the x axis
         θ3 = mapreduce(hcat, collect(1:size(brads, 1))) do i
             atan.(our_pos[:, 2] .- blocks[i][2], our_pos[:, 1] .- blocks[i][1])
         end
-        # println(size(θ3))
-        # distance between us and obs
-        # bdist = sqrt.((our_pos[:,1] .- block[1]).^2 .+ (our_pos[:,2] .- block[2]).^2)
+        # The distance between us and blocks
         bdist = mapreduce(hcat, collect(1:size(brads, 1))) do i
             sqrt.((our_pos[:,1] .- blocks[i][1]).^2 .+ (our_pos[:,2] .- blocks[i][2]).^2)
         end
-        # println(size(bdist))
-        # us to the center of obs, and us to the tangent of obs
-        # θbrad = atan.(brad, bdist)
+        # The angle between 1. the line connecting us to the center of block, and 2. the tangent line to the block that passes through us
         θbrad = mapreduce(hcat, collect(1:size(brads, 1))) do i
             atan.(brads[i], bdist[:, i])
         end
-        # println(size(θbrad))
-        # dθ2 = abs.(angdiff.(θ3, θ2))
+        # The angle between 1. the line connecting us to the center of block, and 2. the line connecting two players
         dθ2 = mapreduce(hcat, collect(1:size(brads, 1))) do i
             abs.(angdiff.(θ3[:, i], θ2))
         end
-        # println(size(dθ2))
+        # true if uncovered, false otherwise.
         uncov = all(collect(1:size(brads, 1))) do i
             (θbrad[:, i] < dθ2[:, i]) || (bdist[:, i] > sqrt.((our_pos[:,1] .- their_pos[:,1]).^2 .+ (our_pos[:,2] .- their_pos[:,2]).^2))
         end
-        
-        # if !uncov
-        #     println(uncov)
-        # end
-        # uncov = (θbrad < dθ2) || (bdist > sqrt.((our_pos[:,1] .- their_pos[:,1]).^2 .+ (our_pos[:,2] .- their_pos[:,2]).^2))
         dθ = angdiff.(θ1, θ2)
         σ = get_sensing_noise.(dθ, uncov)
         μ = state_dist[id_other_pos]
@@ -84,7 +58,6 @@ function make_hastag_sensing_step(agent, other; fov, scale=100.0, offset=0.5, bl
     function rollout_observation(state_dist::StateDist, game_params)
         μ, σ2 = get_gaussian_params(state_dist)   
         obs = sample_trimmed_gauss.(μ, σ2; l=-40, u=40)
-
         alter(state_dist, 
             id_obs => obs
         )
@@ -94,8 +67,8 @@ function make_hastag_sensing_step(agent, other; fov, scale=100.0, offset=0.5, bl
         μ, σ2 = get_gaussian_params(state_dist)   
         x = compare_dist[id_obs]
 
-        # mu:    (|dist| x 2) => (|dist| x 1 x 2)
-        # sigma: (|dist| x 1) => (|dist| x 1 x 1)
+        # μ:    (|dist| x 2) => (|dist| x 1 x 2)
+        # σ2: (|dist| x 1) => (|dist| x 1 x 1)
         # x:     (|info| x 2) => (1 x |info| x 2)
         μ = reshape(μ, (length(state_dist), 1, 2))
         σ2 = reshape(σ2, (length(state_dist), 1, 1))
@@ -121,30 +94,24 @@ function make_hastag_costs(blocks=de_blocks, brads=de_brads)
                 cost_obstacle.(sqrt.(sum((distr[:p1_pos] .- blocks[i]).^2, dims=2)), brads[i])
             end
             total_cost_obstacle = sum(cost_each_obstacle, dims=2)
-            # total_cost_obstacle = mapreduce(i -> cost_obstacle.(sqrt.(sum((distr[:p1_pos] .- blocks[i]).^2, dims=2)), brads[i]), +, 1:size(brads, 1))
             sum((
                 sum((distr[:p1_pos] .- distr[:p2_pos]).^2, dims=2) .+
                 cost_bound.(sqrt.(sum((distr[:p1_pos]).^2, dims=2)), [0], [40]) .+
                 total_cost_obstacle
-                # cost_obstacle.(sqrt.(sum((distr[:p1_pos] .- blocks[12]).^2, dims=2)), brads[12]) .+
-                # cost_obstacle.(sqrt.(sum((distr[:p1_pos] .- blocks[12]).^2, dims=2)), brads[12])
             ) .* exp.(distr.w))
         end
     end
+
     function cost2(hist)
         sum(hist) do distr
             cost_each_obstacle = mapreduce(hcat, collect(1:size(brads, 1))) do i
                 cost_obstacle.(sqrt.(sum((distr[:p2_pos] .- blocks[i]).^2, dims=2)), brads[i])
             end
             total_cost_obstacle = sum(cost_each_obstacle, dims=2)
-            # total_cost_obstacle = mapreduce(i -> cost_obstacle.(sqrt.(sum((distr[:p2_pos] .- blocks[i]).^2, dims=2)), brads[i]), +, 1:size(brads, 1))
-            
             sum((
                 -sum((distr[:p1_pos] .- distr[:p2_pos]).^2, dims=2) .+
                 cost_bound.(sqrt.(sum((distr[:p2_pos]).^2, dims=2)), [0], [40]) .+
                 total_cost_obstacle
-                # cost_obstacle.(sqrt.(sum((distr[:p2_pos] .- blocks[1]).^2, dims=2)), brads[1]) .+
-                # cost_obstacle.(sqrt.(sum((distr[:p2_pos] .- blocks[12]).^2, dims=2)), brads[12])
             ) .* exp.(distr.w))
         end
     end
@@ -165,149 +132,12 @@ function make_hastag_prior(zero_state; n=16)
     end
 end
 
-function test_hastag()
-    T = 5
-    fov = (; p1=1.0, p2=1.0)
-
-    optimization_options = (;
-        n_lookahead = T,
-        n_iters = 1,
-        batch_size = 20,
-        max_wall_time = 120000, # Not respected right now
-        steps_per_seed = 1,
-        steps_per_render = 10
-    )
-
-    clock_step = make_clock_step(1.0)
-
-    p1_pos_hist_step = make_hist_step(:p1_pos_h, :p1_pos, T)
-    p2_pos_hist_step = make_hist_step(:p2_pos_h, :p2_pos, T)
-    p1_obs_hist_step = make_hist_step(:p1_obs_h, :p1_obs, T)
-    p2_obs_hist_step = make_hist_step(:p2_obs_h, :p2_obs, T)
-    p1_ang_hist_step = make_hist_step(:p1_θ_h, :p1_θ, T)
-    p2_ang_hist_step = make_hist_step(:p2_θ_h, :p2_θ, T)
-
-    p1_dynamics_step = make_vel_dynamics_step(:p1; control_scale=1.4)
-    p2_dynamics_step = make_vel_dynamics_step(:p2; control_scale=2)
-
-    p1_bound_step = make_bound_step(:p1_pos, -42, 42)
-    p2_bound_step = make_bound_step(:p2_pos, -42, 42)
-
-    p1_obs_step = make_hastag_sensing_step(:p1, :p2; fov=fov[:p1])
-    p2_obs_step = make_hastag_sensing_step(:p2, :p1; fov=fov[:p2])
-
-    # TODO - there's some special casing for this example in policies.jl that needs to go
-    p1_control_step = make_nn_control(:p1, [:p1_pos_h, :p1_obs_h], :p1_vel, t_max=T)
-    p2_control_step = make_nn_control(:p2, [:p2_pos_h, :p2_obs_h], :p2_vel, t_max=T)
-
-    
-
-    attr_ids = [:pos, :vel, :acc, :θ, :vω, :obs, :pos_h, :obs_h, :θ_h]
-    attr_w   = [ 2;    2;    2;    1;  2;   2;    2*T;    2*T;    T]
-    zero_state = State([
-        :t => 1;
-        Symbol.(:p1, :_, attr_ids) .=> attr_w;
-        Symbol.(:p2, :_, attr_ids) .=> attr_w;
-    ]...)
-
-
-
-    hastag_game = ContinuousGame([
-        clock_step,
-        p1_obs_step,       p2_obs_step,
-        p1_pos_hist_step,  p2_pos_hist_step,
-        p1_obs_hist_step,  p2_obs_hist_step,
-        p1_ang_hist_step,  p2_ang_hist_step,
-        p1_control_step,   p2_control_step,
-        p1_dynamics_step,  p2_dynamics_step,
-        # p1_bound_step,     p2_bound_step
-    ])
-
-    # Version of the game where players don't get new information
-    #   during planning (no active information gathering)
-    inactive_hastag_game = ContinuousGame([
-        clock_step,
-        p1_obs_step,       p2_obs_step,
-        p1_pos_hist_step,  p2_pos_hist_step,
-        p1_ang_hist_step,  p2_ang_hist_step,
-        p1_control_step,   p2_control_step,
-        p1_dynamics_step,  p2_dynamics_step,
-        # p1_bound_step,     p2_bound_step
-    ])
-
-
-
-    prior_fn = make_hastag_prior(zero_state, n=1000)
-    p1_belief = HybridParticleBelief(hastag_game, prior_fn(), 0.1)
-    p2_belief = HybridParticleBelief(hastag_game, prior_fn(), 0.1)
-
-    true_state = StateDist([prior_fn()[rand(1:10)]])
-    p1_ids = [:t; :p1_pos; :p1_vel; :p1_θ; :p1_obs; :p1_pos_h; :p1_obs_h]
-    p2_ids = [:t; :p2_pos; :p2_vel; :p2_θ; :p2_obs; :p2_pos_h; :p2_obs_h]
-
-    cost_fns  = make_hastag_costs()
-    renderer = MakieRenderer()
-
-
-    p1_params = (;
-        p1 = make_policy(4*T, 3; t_max=T), # π_1^(1)
-        p2 = make_policy(4*T, 3; t_max=T)  # π_2^(1)
-    )
-    p2_params = (;
-        p1 = make_policy(4*T, 3; t_max=T), # π_1^(2)
-        p2 = make_policy(4*T, 3; t_max=T)  # π_2^(2)
-    )
-    true_params = (;
-        p1 = p1_params[:p1],
-        p2 = p2_params[:p2]
-    )
-
-    for t in 1:1000
-
-        # Each player solves their game
-        iter = 0
-        p1_params = solve(hastag_game, p1_belief, true_params, cost_fns, optimization_options) do params
-
-            # print(".")
-            iter += 1
-            if iter % optimization_options.steps_per_render != 0
-                return false
-            end
-
-            return false
-        end
-
-        p2_params = solve(hastag_game, p2_belief, true_params, cost_fns, optimization_options)
-        true_params = (;
-            p1 = p1_params[:p1],
-            p2 = p2_params[:p2]
-        )
-
-        #Run variance numbers
-        #Player 1's beliefs about their position
-        #p1_belief.dist[:p1_pos]
-        #Player 1's beliefs about player 2's position
-        #p1_belief.dist[:p2_pos]
-
-        true_state = step(hastag_game, true_state, true_params)
-        update!(p1_belief, select(true_state, p1_ids...)[1], p1_params)
-        update!(p2_belief, select(true_state, p2_ids...)[1], p2_params)
-
-        # Big assumption: p1_params = p2_params = true_params
-        render_hastag(renderer, [
-            (draw(p1_belief; n=20), true_params),
-            (true_state, true_params),
-            (draw(p2_belief; n=20), true_params)
-            ], hastag_game, fov; T)
-    end
-end
-
 function test_hastag_active_info_gathering()
     open("results.txt", "w") do io
         for (p1_solver, p2_solver) in [
-            # (:active, :active),
-            # (:inactive, :active),
-            # (:active, :inactive),
+            (:active, :active),
+            (:inactive, :active),
+            (:active, :inactive),
             (:inactive, :inactive)
             ]
 
@@ -347,11 +177,8 @@ function test_hastag_active_info_gathering()
                 p1_obs_step = make_hastag_sensing_step(:p1, :p2; fov=fov[:p1])
                 p2_obs_step = make_hastag_sensing_step(:p2, :p1; fov=fov[:p2])
 
-                # TODO - there's some special casing for this example in policies.jl that needs to go
                 p1_control_step = make_nn_control(:p1, [:p1_pos_h, :p1_obs_h], :p1_vel, t_max=T)
                 p2_control_step = make_nn_control(:p2, [:p2_pos_h, :p2_obs_h], :p2_vel, t_max=T)
-
-                
 
                 attr_ids = [:pos, :vel, :acc, :θ, :vω, :obs, :pos_h, :obs_h, :θ_h]
                 attr_w   = [ 2;    2;    2;    1;  2;   2;    2*T;    2*T;    T]
@@ -361,8 +188,6 @@ function test_hastag_active_info_gathering()
                     Symbol.(:p2, :_, attr_ids) .=> attr_w;
                 ]...)
 
-
-
                 hastag_game = ContinuousGame([
                     clock_step,
                     p1_obs_step,       p2_obs_step,
@@ -371,10 +196,7 @@ function test_hastag_active_info_gathering()
                     p1_ang_hist_step,  p2_ang_hist_step,
                     p1_control_step,   p2_control_step,
                     p1_dynamics_step,  p2_dynamics_step,
-                    # p1_bound_step,     p2_bound_step
                 ])
-
-
                 
                 planning_hastag_game = ContinuousGame([
                     clock_step,
@@ -386,9 +208,6 @@ function test_hastag_active_info_gathering()
                     p1_dynamics_step,  p2_dynamics_step,
                 ])
 
-
-
-
                 prior_fn = make_hastag_prior(zero_state, n=1000)
                 p1_belief = HybridParticleBelief(hastag_game, prior_fn(), 0.01)
                 p2_belief = HybridParticleBelief(hastag_game, prior_fn(), 0.01)
@@ -398,7 +217,6 @@ function test_hastag_active_info_gathering()
                 p2_ids = [:t; :p2_pos; :p2_vel; :p2_θ; :p2_obs; :p2_pos_h; :p2_obs_h]
 
                 cost_fns  = make_hastag_costs()
-
 
                 p1_params = (;
                     p1 = make_policy(4*T, 3; t_max=T), # π_1^(1)
@@ -418,8 +236,6 @@ function test_hastag_active_info_gathering()
                 n_steps = 40
 
                 for t in 1:n_steps
-                    # print(t)
-
                     # Each player solves their game
                     iter = 0
                     p1_params = solve(planning_hastag_game, p1_belief, true_params, cost_fns, optimization_options) do params
@@ -439,21 +255,13 @@ function test_hastag_active_info_gathering()
                         p2 = p2_params[:p2]
                     )
             
-                    #Run variance numbers
-                    #Player 1's beliefs about their position
-                    #p1_belief.dist[:p1_pos]
-                    #Player 1's beliefs about player 2's position
-                    #p1_belief.dist[:p2_pos]
-            
                     true_state = step(hastag_game, true_state, true_params)
                     update!(p1_belief, select(true_state, p1_ids...)[1], true_params)
                     update!(p2_belief, select(true_state, p2_ids...)[1], true_params)
                     scores = [scores; dist(true_state[1][:p1_pos], true_state[1][:p2_pos])]
                     
                     render_hastag(renderer, [
-                        # (draw(p1_belief; n=20), true_params),
                         (true_state, true_params),
-                        # (draw(p2_belief; n=20), true_params)
                         ], hastag_game, fov; T)
                 end
                 println(io, sum(scores)/n_steps)
@@ -490,12 +298,10 @@ function render_hastag(renderer, dists, game, fov; block_pos=de_blocks, block_r=
                 current_state = dist[i]
                 lik = exp(dist.w[i])
 
-
                 past = unspool(current_state, unspool_scheme...)
                 future_particle = plan[i]
                 future = unspool(future_particle, unspool_scheme...)
                 history = [past; future]
-                # history = [past; future[1]]
 
                 render_trajectory(renderer, history, :p1_pos; 
                     ax_idx=(1, col), color=:blue, alpha=lik, linewidth=4)
@@ -505,8 +311,6 @@ function render_hastag(renderer, dists, game, fov; block_pos=de_blocks, block_r=
                 for (i, state) in enumerate(history)
                     t = i-T
 
-                    # Only plot observations if there are few enough of them
-                    #   that it doesn't get crazy
                     if length(dist) < 5
                         render_location(renderer, state, :p1_obs; 
                             ax_idx=(1, col), color=:blue, alpha=1.0 * ((t > 0) ? 0 : 1), marker='x', markersize=16)
